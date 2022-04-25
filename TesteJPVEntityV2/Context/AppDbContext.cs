2d4cb38811b070adf026df2819102fc36db72394
Lucas Ferreira <luhvf.sfc@gmail.com>
2022-04-24 21:27:02 -0300

    Created Cliente Model and AppDbContext

---
+++ TesteJPVEntityV2/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteJPVEntityV2.Models;
+
+namespace TesteJPVEntityV2.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ TesteJPVEntityV2/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteJPVEntityV2.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public decimal ValorRenda { get; set; }
+    }
+}
